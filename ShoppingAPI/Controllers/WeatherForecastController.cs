6b0af21964bc1158b67623fab6712d0405365e50
JanMichaelSe <janm.montalvo@gmail.com>
2020-08-14 09:26:59 -0400

    Testing Git in visual studio 2019

--- ShoppingAPI/Controllers/WeatherForecastController.cs
+++ ShoppingAPI/Controllers/WeatherForecastController.cs
@@ -33,7 +33,7 @@ namespace ShoppingAPI.Controllers
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
-            .ToArray();
+            .ToArray().Take(1);
         }
     }
 }
